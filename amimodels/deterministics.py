added gamma precision posterior calculation; fixed posterior sampling step method

amimodels/deterministics.py
@@ -17,6 +17,7 @@ NumpyChoose = deterministic_from_funcs("choose", np.choose)
 NumpyPut = deterministic_from_funcs("put", np.put)
 NumpyStack = deterministic_from_funcs("stack", np.stack)
 NumpyHstack = deterministic_from_funcs("hstack", np.hstack)
+NumpyAlen = deterministic_from_funcs("alen", np.alen)
 NumpySum = pymc.NumpyDeterministics.sum
 
 # XXX: See below for likely similar issues.
@@ -62,6 +63,47 @@ class NumpyBroadcastTo(pymc.Deterministic):
 NumpyMultidot = deterministic_from_funcs("multi_dot", np.linalg.multi_dot)
 
 
+class MergeIndexed(pymc.Deterministic):
+    r""" A deterministic that merges multiple indexed arrays
+    into a single array.
+
+    XXX: Can't trace these Deterministics when they change shape.
+    """
+    def __init__(self, arrays, indices, *args, **kwds):
+        r"""
+        Parameters
+        ==========
+        arrays: list/tuple of pymc.Node or numpy.ndarray
+            Array-like objects to merge
+        indices: list/tuple of pymc.Node or numpy.ndarray
+            Indices for the arrays in ``arrays``.
+        """
+        if not all(np.issubdtype(getattr(i_, 'dtype', type(i_)),
+                                 np.integer) for i_ in indices):
+            raise ValueError("indices must be int types")
+
+        parents = {'values_': arrays, 'indices_': indices}
+
+        def merge_by_indices(values_=arrays,
+                             indices_=indices):
+            m_values = np.hstack(values_)
+            m_indices = np.hstack(indices_)
+            res = np.empty(np.shape(m_indices))
+            np.put(res, m_indices, m_values)
+            return res
+
+        name = "Merge({},{})".format(getattr(arrays, '__name__', 'arrays'),
+                                     getattr(indices, '__name__', 'indices'))
+
+        super(MergeIndexed, self).__init__(eval=merge_by_indices,
+                                           doc=self.__doc__,
+                                           name=name,
+                                           parents=parents,
+                                           #trace=False,
+                                           #dtype=np.uint,
+                                           *args, **kwds)
+
+
 class KIndex(pymc.Deterministic):
     r""" A deterministic that dynamically tracks which time/first axis
     indices correspond to a given integer value.
@@ -70,7 +112,8 @@ class KIndex(pymc.Deterministic):
     any pymc.Node that is being indexed.  That means we don't create
     duplicates for the same k-index.
 
-    XXX: Can't trace these Deterministics, since they change shape.
+    XXX: Can't trace these Deterministics when they change shape.
+    This might be a good reason for using a boolean mask instead.
     """
     def __new__(cls, k, states, *args, **kwds):
         if isinstance(states, pymc.Node):
@@ -106,7 +149,8 @@ class KIndex(pymc.Deterministic):
         def k_idx(k, states):
             return np.flatnonzero(k == states)
 
-        name = "which_{}_eq_{}".format(states.__name__, k)
+        name = "which_{}_eq_{}".format(getattr(states, '__name__', 'states'),
+                                       getattr(k, '__name__', k))
 
         super(KIndex, self).__init__(eval=k_idx, doc=self.__doc__,
                                      name=name, parents=parents,
@@ -221,10 +265,24 @@ class HMMLinearCombination(pymc.Deterministic):
 
 
 def get_indexed_items(node):
+    """ Returns a tuple of indices and the partitions (of
+    some array) corresponding to those indices.
+    """
     res = None, None
     if isinstance(node, NumpyTake):
         idx = node.parents['indices']
         col = node.parents['a']
+        res = (idx, col)
+    elif isinstance(node, NumpyHstack):
+        col = node.parents['tup']
+
+        def ranges(parts):
+            n = 0
+            for p in parts:
+                yield range(n, n + len(p))
+                n += len(p)
+
+        idx = list(ranges(col))
         res = (idx, col)
     elif isinstance(node, NumpyChoose):
         idx = node.parents['a']
@@ -233,6 +291,10 @@ def get_indexed_items(node):
     elif isinstance(node, HMMLinearCombination):
         idx = node.states
         col = node.k_prods
+        res = (idx, col)
+    elif isinstance(node, MergeIndexed):
+        idx = node.parents['indices_']
+        col = node.parents['values_']
         res = (idx, col)
     elif "index" in node.parents.keys():
         idx = node.parents['index']

amimodels/normal_hmm.py
@@ -14,7 +14,7 @@ import pymc
 from .stochastics import HMMStateSeq, TransProbMatrix
 from .hmm_utils import compute_trans_freqs, compute_steady_state
 from .deterministics import (HMMLinearCombination, KIndex, NumpyTake,
-                             NumpyChoose, NumpyHstack)
+                             NumpyChoose, MergeIndexed)
 
 
 def get_stochs_excluding(stoch, excluding):
@@ -758,8 +758,7 @@ def make_normal_hmm(y_data, X_data, initial_params=None, single_obs_var=False,
     lambdas = pymc.TupleContainer(lambda_list)
     beta_taus = pymc.TupleContainer(beta_tau_list)
 
-    mu = NumpyHstack(mu_k_list)
-    mu.keep_trace = True
+    mu = MergeIndexed(mu_k_list, state_obs_idx, trace=True)
     #mu = HMMLinearCombination('mu', X_data, betas, states)
 
     if np.alen(V_invs) == 1:

tests/test_graph_ops.py
@@ -9,6 +9,8 @@ import pymc
 import pytest
 from numpy.testing import assert_array_equal
 
+from amimodels.testing import assert_hpd
+
 from amimodels.graph import parse_node_partitions, normal_node_update
 from amimodels.normal_hmm import make_normal_hmm
 
@@ -108,6 +110,9 @@ def test_collapse(hmm_masked):
 
     # This is called 'lam' when the marginal is t-distributed.
     true_marg_tau = np.fromiter((
+        # The marginal is a t-dist when taus are gamma.
+        (2 * taus[s_].parents['alpha']/taus[s_].parents['beta']
+         if isinstance(taus[s_], pymc.Gamma) else 1) *
         np.reciprocal(1 / pymc.utils.value(y_rv.parents['tau']) +
                       np.sum(np.square(X_matrices[s_].iloc[t_]) /
                              pymc.utils.value(betas[s_].parents['tau'])))
@@ -136,7 +141,8 @@ def test_collapse_norm_hmm():
     np.random.seed(2352523)
     N_obs = 20
     #y_obs = pd.Series(np.arange(N_obs))
-    y_obs = pd.Series(np.zeros(N_obs))
+    #y_obs = pd.Series(np.zeros(N_obs))
+    y_obs = pd.Series(np.ones(N_obs))
     X_matrices = [pd.DataFrame(np.ones((N_obs, 1))),
                   pd.DataFrame(np.ones((N_obs, 2)))]
 
@@ -150,13 +156,15 @@ def test_collapse_norm_hmm():
     normal_node_update(node, updates)
 
     y_marginal, = norm_hmm.y_rv.marginals
+    posteriors = tuple(getattr(n_, 'posterior') for n_ in norm_hmm.nodes
+                       if hasattr(n_, 'posterior'))
 
     # Get rid of the dependencies on this node
     # and create a new model with the marginal
     # replacing the un-marginalized.
     #obs_node.parents.detach_extended_parents()
     y_marginal.parents['mu'].keep_trace = True
-    new_nodes = set((y_marginal, y_marginal.parents['mu'])) |\
+    new_nodes = set((y_marginal, y_marginal.parents['mu']) + posteriors) |\
         y_marginal.extended_parents
 
     #_ = [n_ for n_ in new_nodes]
@@ -164,17 +172,27 @@ def test_collapse_norm_hmm():
 
     mcmc_step = pymc.MCMC(new_norm_hmm.variables)
 
+    mcmc_step.use_step_method(pymc.StepMethods.Metropolis,
+                              new_norm_hmm.get_node('eta-1'),
+                              proposal_sd=1e-1,
+                              proposal_distribution=None
+                              )
+    mcmc_step.use_step_method(pymc.StepMethods.Metropolis,
+                              new_norm_hmm.get_node('lambdas-0'),
+                              proposal_sd=1e-1,
+                              proposal_distribution=None
+                              )
+    mcmc_step.use_step_method(pymc.StepMethods.Metropolis,
+                              new_norm_hmm.get_node('lambdas-1'),
+                              proposal_sd=1e-1,
+                              proposal_distribution=None
+                              )
     mcmc_step.assign_step_methods()
     mcmc_step.step_method_dict
 
-    mcmc_step.sample(200)
+    mcmc_step.sample(4000)
 
-    mcmc_step.db.trace_names
-
-    new_norm_hmm.get_node('states').stats()
-    new_norm_hmm.get_node('mu_y_marg').stats()
-    #y_marginal.parents['mu'].trace()
-
+    assert_hpd(new_norm_hmm.get_node('mu_y_marg'), y_obs)
 
 @pytest.mark.skip(reason="In progress...")
 def test_collapse_binomial():